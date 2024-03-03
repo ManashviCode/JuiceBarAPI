28777b7292775728417f9d9527f8c274daa87a82
Divani Perera <divamanashvi03@gmail.com>
2024-03-04 01:38:14 +0530

    2nd initial commit

--- JuiceBarAPI/Program.cs
+++ JuiceBarAPI/Program.cs
@@ -23,12 +23,12 @@ builder.Services.AddSwaggerGen();*/
 
 var app = builder.Build();
 
-/*// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}*/
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
